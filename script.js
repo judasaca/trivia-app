79dea298027e1ef01b4c5e74b59aa509eedcd1dd
judasaca2 <juandavid@vuedata.com>
2023-07-27 18:57:28 -0500

    Add categories options from API connection.

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-async function getCategories() {
-  let categories = await fetch("https://opentdb.com/api_category.php");
-  categories = await categories.json();
-  return categories.trivia_categories;
-}
-
 async function getData(difficulty, answer_type, id_categoria) {
   let questions = await fetch(
     `https://opentdb.com/api.php?amount=10&category=${id_categoria}&difficulty=${difficulty}&type=${answer_type}`
@@ -12,3 +6,20 @@ async function getData(difficulty, answer_type, id_categoria) {
   //   console.log(questions);
   return questions.results;
 }
+async function getCategories() {
+  let categories = await fetch("https://opentdb.com/api_category.php");
+  categories = await categories.json();
+  return categories.trivia_categories;
+}
+
+const category_options = document.getElementById("categorySelect");
+
+getCategories().then((categories) => {
+  categories.forEach((element) => {
+    let { id, name } = element;
+    const option = document.createElement("option");
+    option.setAttribute("value", id);
+    option.textContent = name;
+    category_options.appendChild(option);
+  });
+});
